fe25de1f5512420dea7926a51fda61820df3024c
Pakornsak Supamongkonchaikul <pakornsak.sup@ascendcorp.com>
2021-08-15 02:24:03 +0700

    Add ball model

---
+++ public/ball.js
@@ -0,0 +1,20 @@
+//@ts-check
+/**
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+const rand = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+class Ball {
+  constructor() {
+    const r = rand(1, 10) * 10;
+
+    this.x = rand(r, 576 - r);
+    this.y = 0;
+    this.radius = r;
+    this.point = r / 10;
+  }
+}
--- public/board.js
+++ public/board.js
@@ -5,9 +5,27 @@ class Board {
    */
   constructor(ctx) {
     this.ctx = ctx;
+    /** @type {Ball[]}*/
+    this.balls = [];
+    this.balls.push(new Ball());
+    this.balls.push(new Ball());
+  }
+
+  /**
+   * Draw a ball
+   * @param {Ball} ball
+   */
+  drawBall(ball) {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#c3fff3";
+    ctx.fill();
+    ctx.closePath();
   }
 
   draw() {
-    //
+    for (let i = 0; i < this.balls.length; i++) {
+      this.drawBall(this.balls[i]);
+    }
   }
 }
