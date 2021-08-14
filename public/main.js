53f9a16871ab04e937ff98c34ad4b3b900e3c55e
Pakornsak Supamongkonchaikul <pakornsak.sup@ascendcorp.com>
2021-08-15 01:33:59 +0700

    Update css

--- public/main.js
+++ public/main.js
@@ -1,8 +1,10 @@
 //@ts-check
+
 /** @type {HTMLCanvasElement} */
 // @ts-ignore
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
+const silder = document.getElementById("slider");
 
 class Game {
   constructor(ctx) {
@@ -14,15 +16,29 @@ class Game {
     this.score = 0;
   }
 
-  animate(now = 0) {
+  animate = () => {
     this.board.draw();
     this.requestId = requestAnimationFrame(this.animate);
-  }
+  };
 
-  play() {
+  play = () => {
     this.animate();
-  }
+  };
+
+  /**
+   * @param {string} speed
+   */
+  updateSpeed = (speed) => {
+    this.speed = Number(speed);
+    document.getElementById("speed").textContent = speed;
+  };
+
+  // updateScore = (x) => {
+
+  // }
 }
 
 const game = new Game(ctx);
 game.play();
+
+silder.oninput = (e) => game.updateSpeed(e.target.value);
